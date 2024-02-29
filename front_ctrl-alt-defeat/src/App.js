68eeb07798055fcc9055786256668dd32228fa4f
DouglasSouza05 <douglasbsouza01@gmail.com>
2024-02-29 17:24:51 -0300

    Subindo correções, alterações e organização do Front

--- front_ctrl-alt-defeat/src/App.js
+++ front_ctrl-alt-defeat/src/App.js
@@ -1,94 +1,131 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; // Importe o arquivo CSS
+import React, { useState } from "react";
+import "./App.css";
 
 const Login = () => {
-  const [aba, setAba] = useState('equipe');
-  const [equipe, setEquipe] = useState([{ email: '', matricula: '' }]);
-  const [emailOrientador, setEmailOrientador] = useState('');
-  const [senhaOrientador, setSenhaOrientador] = useState('');
+  const [aba, setAba] = useState("equipe");
+  const [equipe, setEquipe] = useState([
+    { nome: "", email: "", matricula: "" },
+  ]);
+  const [nomeOrientador, setNomeOrientador] = useState("");
+  const [emailOrientador, setEmailOrientador] = useState("");
+  const [senhaOrientador, setSenhaOrientador] = useState("");
 
-  useEffect(() => {
-    if (aba === 'equipe' && equipe.length === 0) {
-      setEquipe([{ email: '', matricula: '' }]); // Adiciona um membro por padrão se a aba é "equipe" e não há membros
-    }
-  }, [aba, equipe]);
-
-  const mudarAba = (aba) => {
-    setAba(aba);
+  const mudarAba = (novaAba) => {
+    setAba(novaAba);
   };
 
   const mudarMembro = (indice, campo, valor) => {
-    const novaEquipe = equipe.map((membro, i) => {
-      if (i === indice) {
-        return { ...membro, [campo]: valor };
-      }
-      return membro;
-    });
+    const novaEquipe = equipe.map((membro, i) =>
+      i === indice ? { ...membro, [campo]: valor } : membro
+    );
     setEquipe(novaEquipe);
   };
 
   const adicionarMembro = () => {
     if (equipe.length < 4) {
-      setEquipe([...equipe, { email: '', matricula: '' }]);
+      setEquipe([...equipe, { nome: "", email: "", matricula: "" }]);
     }
   };
 
   const removerMembro = (indice) => {
-    const novaEquipe = [...equipe];
-    novaEquipe.splice(indice, 1);
+    const novaEquipe = equipe.filter((_, i) => i !== indice);
     setEquipe(novaEquipe);
   };
 
-  const validarMatricula = (valor) => {
-    return /^\d*$/.test(valor); // Verifica se a matrícula contém apenas números inteiros
-  };
+  const validarMatricula = (valor) => /^\d*$/.test(valor);
+
+  const validarEmail = (valor) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(valor);
+
+  const validarSenha = (valor) => valor.length >= 6;
 
   const cadastrarEquipe = () => {
-    console.log('Dados dos membros da equipe:');
+    console.log("Dados dos Membros da Equipe: ");
     console.log(equipe);
   };
 
   const cadastrarOrientador = () => {
-    console.log('Dados do orientador:');
-    console.log('Email: ', emailOrientador);
-    console.log('Senha: ', senhaOrientador);
+    if (validarEmail(emailOrientador) && validarSenha(senhaOrientador)) {
+      console.log("Dados do orientador: ");
+      console.log("Nome: ", nomeOrientador);
+      console.log("Email: ", emailOrientador);
+      console.log("Senha: ", senhaOrientador);
+    } else {
+      console.log("Email ou Senha Inválidos!");
+    }
   };
 
   return (
-    <div className="login-container"> {/* Adicione a classe CSS ao container */}
-      <button className={`tab-btn ${aba === 'equipe' ? 'active' : ''}`} onClick={() => mudarAba('equipe')}>Equipe</button>
-      <button className={`tab-btn ${aba === 'orientador' ? 'active' : ''}`} onClick={() => mudarAba('orientador')}>Orientador</button>
-      {aba === 'equipe' && (
-        <div>
+    <div className="login-container">
+      <div className="tab-btn">
+        <button
+          className={aba === "equipe" ? "active" : ""}
+          onClick={() => mudarAba("equipe")}
+        >
+          Equipe
+        </button>
+        <button
+          className={aba === "orientador" ? "active" : ""}
+          onClick={() => mudarAba("orientador")}
+        >
+          Orientador
+        </button>
+      </div>
+      {aba === "equipe" && (
+        <div className={`equipe-section ${aba === "equipe" ? "active" : ""}`}>
           {equipe.map((membro, indice) => (
             <div key={indice} className="membro-equipe">
               <input
                 type="text"
-                placeholder={`Email do Membro da Equipe ${indice + 1}`}
-                value={membro.email}
-                onChange={(e) =>
-                  mudarMembro(indice, 'email', e.target.value)
-                }
+                placeholder={`Nome do Membro ${indice + 1}`}
+                value={membro.nome}
+                onChange={(e) => mudarMembro(indice, "nome", e.target.value)}
               />
               <input
-                type="text"
-                placeholder={`Matrícula do Membro da Equipe ${indice + 1}`}
+                type="email"
+                placeholder={`Email do Membro ${indice + 1}`}
+                value={membro.email}
+                onChange={(e) => mudarMembro(indice, "email", e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder={`Matrícula do Membro ${indice + 1}`}
                 value={membro.matricula}
                 onChange={(e) =>
-                  validarMatricula(e.target.value) && mudarMembro(indice, 'matricula', e.target.value)
+                  validarMatricula(e.target.value) &&
+                  mudarMembro(indice, "matricula", e.target.value)
                 }
               />
-              {indice > 0 && (
-                <button onClick={() => removerMembro(indice)}>Remover</button>
-              )}
+              <div className="remove-btn">
+                {indice > 0 && (
+                  <button onClick={() => removerMembro(indice)}>Remover</button>
+                )}
+              </div>
             </div>
           ))}
-          <button className="add-btn" onClick={adicionarMembro} disabled={equipe.length === 4}>Adicionar Membro</button>
-          <button className="cadastrar-btn" onClick={cadastrarEquipe}>Cadastrar</button>
+          <button
+            className="add-btn"
+            onClick={adicionarMembro}
+            disabled={equipe.length === 4}
+          >
+            Adicionar Membro
+          </button>
+          <button className="cadastrar-btn" onClick={cadastrarEquipe}>
+            Cadastrar
+          </button>
         </div>
       )}
-      {aba === 'orientador' && (
-        <div>
+      {aba === "orientador" && (
+        <div
+          className={`orientador-section ${
+            aba === "orientador" ? "active" : ""
+          }`}
+        >
+          <input
+            type="text"
+            placeholder="Nome"
+            value={nomeOrientador}
+            onChange={(e) => setNomeOrientador(e.target.value)}
+          />
           <input
             type="email"
             placeholder="Email"
@@ -101,8 +138,12 @@ const Login = () => {
             value={senhaOrientador}
             onChange={(e) => setSenhaOrientador(e.target.value)}
           />
-          <button className="cadastrar-btn" onClick={cadastrarOrientador}>Cadastrar</button>
         </div>
+      )}
+      {aba === "orientador" && (
+        <button className="cadastrar-btn" onClick={cadastrarOrientador}>
+          Cadastrar
+        </button>
       )}
     </div>
   );
