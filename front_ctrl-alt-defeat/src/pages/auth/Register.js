20b5233741972cd18fe93574cc1a37c71e40b753
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2024-03-29 20:07:15 -0300

    Implementacao do redux, configurando requisicoes HTTP, criando um servico de authenticacao e e slice de authenticacao

--- front_ctrl-alt-defeat/src/pages/auth/Register.js
+++ front_ctrl-alt-defeat/src/pages/auth/Register.js
@@ -4,12 +4,26 @@ import "./Auth.css"
 import {Link} from 'react-router-dom'
 
 // Hooks
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 const Register = () => {
 
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setpassword] = useState ("")
+  const [confirmPassword, setconfirmPassword] = useState("")
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user)
   }
 
   return (
@@ -17,10 +31,10 @@ const Register = () => {
       <h2>Ctrl+Alt+Defeat</h2>
       <p className="subtitle"> Cadastre-se para participar da maior feira de tecnologia de Minas Gerais</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome Completo"/>
-        <input type="email" placeholder="Email da instituicao"/>
-        <input type="password" placeholder="Senha"/>
-        <input type="password" placeholder="Confirme a senha"/>
+        <input type="text" placeholder="Nome Completo" onChange={(e) => setName(e.target.value)} value={name || ""}/>
+        <input type="email" placeholder="Email da instituicao" onChange={(e) => setEmail(e.target.value)} value={email || ""}/>
+        <input type="password" placeholder="Senha" onChange={(e) => setpassword(e.target.value)} value={password || ""}/>
+        <input type="password" placeholder="Confirme a senha" onChange={(e) => setconfirmPassword(e.target.value)} value={confirmPassword || ""}/>
         <input type="submit" placeholder="Cadastrar"/>
       </form>
       <p>
---
+++ front_ctrl-alt-defeat/src/services/authService.js
@@ -0,0 +1,20 @@
+import { api, requestConfig } from "../utils/config"
+
+const register = async (data) => {
+    const config = requestConfig("POST", data)
+    try {
+        const res = await fetch(api + "/register/aluno", config).then((res) => res.json()).catch((err) => err)
+
+        if(res){
+            localStorage.setItem("user", JSON.stringify(res))
+        }
+    } catch (err) {
+        console.log('Error in Register: ', err);
+    }
+}
+
+const authService = {
+    register,
+}
+
+export default authService
