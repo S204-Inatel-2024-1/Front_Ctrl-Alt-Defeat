04fdeb784dfc60cc6655a28921f3f609f947a791
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2024-03-29 18:43:12 -0300

    Criando HTML da tela de registro

--- front_ctrl-alt-defeat/src/pages/auth/Register.js
+++ front_ctrl-alt-defeat/src/pages/auth/Register.js
@@ -1,8 +1,29 @@
 import "./Auth.css"
 
+// Components
+import {Link} from 'react-router-dom'
+
+// Hooks
+import { useState, useEffect } from "react"
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>Ctrl+Alt+Defeat</h2>
+      <p className="subtitle"> Cadastre-se para participar da maior feira de tecnologia de Minas Gerais</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome Completo"/>
+        <input type="email" placeholder="Email da instituicao"/>
+        <input type="password" placeholder="Senha"/>
+        <input type="password" placeholder="Confirme a senha"/>
+
+      </form>
+    </div>
   )
 }
 
