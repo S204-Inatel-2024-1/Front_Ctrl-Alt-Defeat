ff798037f65cecd3b85820c011fb60ffc6f1bf91
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2024-06-23 15:58:58 -0300

    Subi a pagina do adm no lugar da pagina do aluno e removendo imagens de login do canto da tela

--- front_ctrl-alt-defeat/src/pages/auth/LoginAluno.js
+++ front_ctrl-alt-defeat/src/pages/auth/LoginAluno.js
@@ -11,56 +11,55 @@ import { useSelector, useDispatch } from "react-redux"
 // Redux
 import { login, reset } from "../../slices/authSlice"
 
-const LoginAdm = () => {
-    const [email, setEmail] = useState("Fetin@inatel.br")
-    const [password, setPassword] = useState("teste")
+const LoginAluno = () => {
+  const [email, setEmail] = useState("eduardo.costa@ges.inatel.br")
+  const [password, setPassword] = useState("teste")
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const { loading, msg, user } = useSelector((state) => state.auth)
+  const { loading, msg, user } = useSelector((state) => state.auth)
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const user = {
-            email,
-            password
-        }
+    const user = {
+      email,
+      password,
+    };
 
-        dispatch(login({ userData: user, route: "admin" }))
+    dispatch(login({ userData: user, route: "aluno" }))
+  };
+
+  // Redirecionar após login bem-sucedido
+  useEffect(() => {
+    if (user === email) {
+      console.log("User do LoginAluno: ", user)
+      navigate(`/ProfileAluno/${user}`); // Redireciona para o perfil do aluno com o email
+    } else {
+      navigate(`/LoginAluno`)
     }
+  }, [user, navigate]);
 
-    // Limpando todos os estados auth
-    useEffect(() => {
-        dispatch(reset())
-    }, [dispatch])
+  // Limpando todos os estados auth
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
-    useEffect(() => {
-        console.log("Usuario: ", user)
-        if (user === email) {
-            console.log("User do LoginAluno: ", email)
-            navigate(`/ProfileAdm/${user}`); // Redireciona para o perfil do aluno com o email
-        }
-        else { 
-            navigate('/LoginAdm')
-        }
-    }, [user, navigate]);
+  return (
+    <div id="login">
+      <h2>Entre como Aluno</h2>
+      <p className="subtitle">Faça login para cadastrar um projeto</p>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+        <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""} />
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {msg && <Message msg={msg} type="error" />}
+      </form>
+      <p>Não se cadastrou ainda? <Link to="/RegisterAluno">Clique aqui</Link></p>
+    </div>
+  );
+};
 
-    return (
-        <div id="login">
-            <h2>Entre como Administrador</h2>
-            <p className="subtitle">Faça login para administrar a feira</p>
-            <form onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email || ""} />
-                <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""} />
-                {!loading && <input type="submit" value="Entrar" />}
-                {loading && <input type="submit" value="Aguarde..." disabled />}
-                {msg && <Message msg={msg} type="error" />}
-            </form>
-            <p>Não se cadastrou ainda? <Link to="/RegisterAdm">Clique aqui</Link></p>
-        </div>
-    )
-}
-
-export default LoginAdm
+export default LoginAluno;
