82c000dd7023e7caae8d80ff2d71cc6d82fea361
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2024-06-23 16:05:13 -0300

    Removendo logins fixos que facilitavam meus testes

--- front_ctrl-alt-defeat/src/pages/auth/LoginAdm.js
+++ front_ctrl-alt-defeat/src/pages/auth/LoginAdm.js
@@ -12,8 +12,8 @@ import { useSelector, useDispatch } from "react-redux"
 import { login, reset } from "../../slices/authSlice"
 
 const LoginAdm = () => {
-    const [email, setEmail] = useState("Fetin@inatel.br")
-    const [password, setPassword] = useState("teste")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
